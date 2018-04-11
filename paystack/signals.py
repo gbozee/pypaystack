497cb9f1dfbfbb148a66df3798fef9bd4b420388
biola <gbozee@gmail.com>
2018-04-11 19:27:21 +0100

    send the order to the successful payment signal

--- paystack/signals.py
+++ paystack/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import Signal
 
-payment_verified = Signal(providing_args=["ref","amount"])
+payment_verified = Signal(providing_args=["ref","amount", "order"])
 
 event_signal = Signal(providing_args=['event', "data"])
