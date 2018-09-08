88fb76210aa2163229e1d5c78cb8047a1e984214
Beesama <gbozee@gmail.com>
2018-09-08 09:14:38 +0100

    moved js_script to api instead of the client

--- paystack/templatetags/paystack.py
+++ paystack/templatetags/paystack.py
@@ -4,6 +4,7 @@ from django.shortcuts import reverse
 from django.utils.html import format_html
 from .. import settings
 from django.utils.crypto import get_random_string
+from paystack.utils import get_js_script
 register = template.Library()
 
 
@@ -24,5 +25,6 @@ def paystack_button(context, button_id="django-paystack-button", button_class=""
         'ref': new_ref,
         'email': email,
         'amount': new_amount,
-        'redirect_url': new_redirect_url
+        'redirect_url': new_redirect_url,
+        'js_url': get_js_script()
     }
--- paystack/utils.py
+++ paystack/utils.py
@@ -6,7 +6,6 @@ import importlib
 
 
 class PaystackAPI(object):
-
     def __init__(self):
         self.public_key = settings.PAYSTACK_PUBLIC_KEY
         self.secret_key = settings.PAYSTACK_SECRET_KEY
@@ -14,26 +13,24 @@ class PaystackAPI(object):
 
     def make_request(self, method, path, **kwargs):
         options = {
-            'GET': requests.get,
-            'POST': requests.post,
-            'PUT': requests.put,
-            'DELETE': requests.delete
+            "GET": requests.get,
+            "POST": requests.post,
+            "PUT": requests.put,
+            "DELETE": requests.delete,
         }
         url = "{}{}".format(self.base_url, path)
-        headers = {
-            'Authorization': "Bearer {}".format(self.secret_key)
-        }
+        headers = {"Authorization": "Bearer {}".format(self.secret_key)}
         return options[method](url, headers=headers, **kwargs)
 
     def verify_result(self, response, **kwargs):
         if response.status_code == 200:
             result = response.json()
-            data = result['data']
-            amount = kwargs.get('amount')
+            data = result["data"]
+            amount = kwargs.get("amount")
             if amount:
-                if data['amount'] == int(amount):
+                if data["amount"] == int(amount):
                     return True, result["message"]
-                return False, data['amount']
+                return False, data["amount"]
             return True, result["message"]
 
         if response.status_code >= 400:
@@ -41,7 +38,7 @@ class PaystackAPI(object):
 
     def verify_payment(self, code, **kwargs):
         path = "/transaction/verify/{}".format(code)
-        response = self.make_request('GET', path)
+        response = self.make_request("GET", path)
         return self.verify_result(response, **kwargs)
 
 
@@ -51,6 +48,10 @@ def load_lib(config=settings.PAYSTACK_LIB_MODULE):
 
 
 def generate_digest(data):
-    return hmac.new(settings.PAYSTACK_SECRET_KEY.encode('utf-8'),
-                    msg=data,
-                    digestmod=hashlib.sha512).hexdigest()  # request body hash digest
+    return hmac.new(
+        settings.PAYSTACK_SECRET_KEY.encode("utf-8"), msg=data, digestmod=hashlib.sha512
+    ).hexdigest()  # request body hash digest
+
+
+def get_js_script():
+    return "https://js.paystack.co/v1/inline.js"
