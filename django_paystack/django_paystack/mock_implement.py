246bd285db37d9e194e835b234139108064a2a64
Beesama <gbozee@gmail.com>
2019-01-04 11:29:18 +0100

    added transfer implementation to the library

--- django_paystack/django_paystack/mock_implement.py
+++ django_paystack/django_paystack/mock_implement.py
@@ -1,9 +1,17 @@
 from paystack.utils import PaystackAPI as MPaystackAPI
+from paystack.api import Transaction
 
-class PaystackAPI(MPaystackAPI):
+
+class TestTransaction(Transaction):
     def verify_result(self, response, **kwargs):
         if response.status_code == 200:
             result = response.json()
             data = result['data']
             return data['amount']
         return "hello"
+
+
+class PaystackAPI(MPaystackAPI):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.transaction_api = TestTransaction(self.make_request)
