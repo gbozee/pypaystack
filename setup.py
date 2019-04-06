e07245f150eea3b71201dc05de29281de70b73c9
Beesama <gbozee@gmail.com>
2019-04-06 04:56:50 +0100

    install from github

--- setup.py
+++ setup.py
@@ -1,36 +1,46 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-paystack',
-    version='0.9.2',
+    name="django-paystack",
+    version="0.9.3",
     packages=find_packages(),
     include_package_data=True,
-    license='MIT License',  # example license
-    description='A reusable app for making online payments with paystack',
+    license="MIT License",  # example license
+    description="A reusable app for making online payments with paystack",
     long_description=README,
-    url='https://www.example.com/',
-    author='Biola Oyeniyi',
-    author_email='gbozee@gmail.com',
-    install_requires=['requests', 'future', 'Paperboy','python-dateutil','requests-async'],
+    url="https://www.example.com/",
+    author="Biola Oyeniyi",
+    author_email="gbozee@gmail.com",
+    install_requires=[
+        "requests",
+        "future",
+        "Paperboy",
+        "python-dateutil",
+        # "requests-async",
+        "requests-async @ https://github.com/encode/requests-async/archive/master.zip",
+    ],
+    # dependency_links=[
+        # "http://github.com/SergeySatskiy/cdm-pythonparser/archive/v2.0.1.tar.gz"
+    # ],
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',  # example license
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: X.Y",  # replace "X.Y" as appropriate
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",  # example license
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
         # Replace these appropriately if you are stuck on Python 2.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 2',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
 )
