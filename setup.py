e2d78b77124c3264da8b33607d66832c4264307f
Beesama <gbozee@gmail.com>
2019-10-18 05:55:21 +0100

    getting the library to support starlette usecase

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="django-paystack",
-    version="0.9.3",
+    name="pypaystack",
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",  # example license
